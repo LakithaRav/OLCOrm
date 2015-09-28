31a0049fe4a885434652f655cf25645d53458db7
LakithaRav <lakitharav@gmail.com>
2015-09-28 18:32:41 +0530

    Unwanted Test frameworks removed

---
+++ Example/Pods/Target Support Files/OLCOrm/OLCOrm-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_OLCOrm : NSObject
+@end
+@implementation PodsDummy_OLCOrm
+@end
--- Example/Pods/Target Support Files/Pods-OLCOrm-OLCOrm/Pods-OLCOrm-OLCOrm-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_OLCOrm_OLCOrm : NSObject
-@end
-@implementation PodsDummy_Pods_OLCOrm_OLCOrm
-@end
